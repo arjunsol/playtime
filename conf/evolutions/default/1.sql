5cae57a7cf96b5bb51eefa8364189dddd1fe99ff
Nick Shaw <nick.shaw@g4me.co.uk>
2014-01-06 20:34:17 +0000

    Play 2.2.1 upgrade

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -7,19 +7,19 @@ create table `application` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`nam
 create table `field` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(254) NOT NULL,`model_id` BIGINT NOT NULL,`field_type` VARCHAR(254) NOT NULL,`required` BOOLEAN NOT NULL,`related_model_id` BIGINT);
 create table `model` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(254) NOT NULL,`application_id` BIGINT NOT NULL);
 create table `module` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(254) NOT NULL,`application_id` BIGINT NOT NULL,`dependencies` BIGINT);
-alter table `field` add constraint `field_related_model_id` foreign key(`related_model_id`) references `model`(`id`) on update NO ACTION on delete NO ACTION;
 alter table `field` add constraint `field_model_id` foreign key(`model_id`) references `model`(`id`) on update NO ACTION on delete NO ACTION;
+alter table `field` add constraint `field_related_model_id` foreign key(`related_model_id`) references `model`(`id`) on update NO ACTION on delete NO ACTION;
 alter table `model` add constraint `model_application_id` foreign key(`application_id`) references `application`(`id`) on update NO ACTION on delete NO ACTION;
-alter table `module` add constraint `Module_Module_dependencies` foreign key(`dependencies`) references `module`(`id`) on update NO ACTION on delete NO ACTION;
 alter table `module` add constraint `module_application_id` foreign key(`application_id`) references `application`(`id`) on update NO ACTION on delete NO ACTION;
+alter table `module` add constraint `Module_Module_dependencies` foreign key(`dependencies`) references `module`(`id`) on update NO ACTION on delete NO ACTION;
 
 # --- !Downs
 
-ALTER TABLE field DROP FOREIGN KEY field_related_model_id;
 ALTER TABLE field DROP FOREIGN KEY field_model_id;
+ALTER TABLE field DROP FOREIGN KEY field_related_model_id;
 ALTER TABLE model DROP FOREIGN KEY model_application_id;
-ALTER TABLE module DROP FOREIGN KEY Module_Module_dependencies;
 ALTER TABLE module DROP FOREIGN KEY module_application_id;
+ALTER TABLE module DROP FOREIGN KEY Module_Module_dependencies;
 drop table `application`;
 drop table `field`;
 drop table `model`;
